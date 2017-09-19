f7b598edb30f335eb0f145deccc961548b291e9d
Marcelo Glacial <glacial@gmail.com>
2017-09-19 18:00:58 -0400

    First commit

---
+++ wordpress/teste.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>
+<?php bloginfo('name'); ?>
+<?php wp_title(''); ?>
+</title>
+<meta http-equiv="Content-Type" content="text/html; charset=<?php bloginfo('charset'); ?>" />
+<meta name="generator" content="WordPress <?php bloginfo('version'); ?>" />
+<meta name="description" content="<?php bloginfo('description'); ?>" />
+<link rel="stylesheet" type="text/css" media="screen" href="<?php bloginfo('stylesheet_url'); ?>" />
+<link rel="icon" href="<?php /*?><!-- add link to favicon --><?php */?>" />
+<link rel="shortcut icon" href="<?php /*?><!-- add link to favicon --><?php */?>" />
+<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS Feed" href="<?php bloginfo('rss2_url'); ?>" />
+<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+</head>
+<body id="Home">
+<!-- SITE -->
+<div id="Site">
+  <!-- CABECALHO -->
+  <div id="Cabecalho">
+    <h1><a href="#" title="Página Inicial">PodSemFio</a></h1>
+    <p class="descricao">O Podcast da Garota Sem Fio</p>
+  </div>
+  <!-- FIM DE CABECALHO -->
+  <hr />
+  <!-- NAVEGACAO -->
+  <div id="Navegacao">
+    <h2>Navegação</h2>
+    <ul>
+      <li><a href="#Posts">Conteúdo</a></li>
+      <li><a href="#About">Sobre a autora</a></li>
+      <li><a href="#Assine">Assine</a></li>
+      <li><a href="#Rodape">Rodapé</a></li>
+    </ul>
+  </div>
+  <!-- FIM DE NAVEGACAO -->
+  <hr />
+  <!-- CONTEUDO -->
+  <div id="Conteudo">
+    <!-- POSTS -->
+    <div id="Posts">
+      <div class="post">
+        <h3 class="titulo"><a href="#" title="Leia o post completo">Podsemfio n.47 - MP3 em celulares (parte I)</a></h3>
+        <p class="data"><span class="dia">11</span><span class="separador">/</span><span class="mes">Set</span><span class="separador">/</span><span class="ano">2007</span></p>
+        <p>Quando se trata de casar celular com diversão, ninguém bate a Sony Ericsson. A melhor conectividade com PDAs. O <a href="#">MP3 player da Ivete Sangalo</a>. Depois da linha Walkman e da linha Cybershot, vem aí mais uma linha de celular com a grife de uma linha de produtos Sony. Alguém sabe qual é?</p>
+        <p>Encerrando com "Something Stupid", com Robbie Williams (garoto-propaganda da Sony Ericsson) em dueto com Nicole Kidman. A música foi feita para um comercial de TV que depois ficou bem conhecido.</p>
+        <p><img src="http://www.odontopalm.com.br/blog/tytn-ii-4.jpg" alt="PDphone" /></p>
+        <p>Quando se trata de casar celular com diversão, ninguém bate a Sony Ericsson. A melhor conectividade com PDAs. O <a href="#">MP3 player da Ivete Sangalo</a>. Depois da linha Walkman e da linha Cybershot, vem aí mais uma linha de celular com a grife de uma linha de produtos Sony. Alguém sabe qual é?</p>
+        <ul>
+          <li>Item</li>
+          <li><a href="#">Item</a></li>
+          <li>Item</li>
+        </ul>
+        <p>Encerrando com "Something Stupid", com Robbie Williams (garoto-propaganda da Sony Ericsson) em dueto com Nicole Kidman. A música foi feita para um comercial de TV que depois ficou bem conhecido.</p>
+        <blockquote>
+          <p>Quando se trata de casar celular com diversão, ninguém bate a Sony Ericsson. A melhor conectividade com PDAs. O <a href="#">MP3 player da Ivete Sangalo</a>. Depois da linha Walkman e da linha Cybershot, vem aí mais uma linha de celular com a grife de uma linha de produtos Sony. Alguém sabe qual é?</p>
+        </blockquote>
+        <p>Encerrando com "Something Stupid", com Robbie Williams (garoto-propaganda da Sony Ericsson) em dueto com Nicole Kidman. A música foi feita para um comercial de TV que depois ficou bem conhecido.</p>
+        <p>Quando se trata de casar celular com diversão, ninguém bate a Sony Ericsson. A melhor conectividade com PDAs. O <a href="#">MP3 player da Ivete Sangalo</a>. Depois da linha Walkman e da linha Cybershot, vem aí mais uma linha de celular com a grife de uma linha de produtos Sony. Alguém sabe qual é?</p>
+      </div>
+      <hr />
+      <div class="post">
+        <h3 class="titulo"><a href="#" title="Leia o post completo">Podsemfio n.47 - MP3 em celulares (parte I)</a></h3>
+        <p class="data"><span class="dia">11</span><span class="separador">/</span><span class="mes">Set</span><span class="separador">/</span><span class="ano">2007</span></p>
+        <p>Quando se trata de casar celular com diversão, ninguém bate a Sony Ericsson. A melhor conectividade com PDAs. O <a href="#">MP3 player da Ivete Sangalo</a>. Depois da linha Walkman e da linha Cybershot, vem aí mais uma linha de celular com a grife de uma linha de produtos Sony. Alguém sabe qual é?</p>
+        <p>Encerrando com "Something Stupid", com Robbie Williams (garoto-propaganda da Sony Ericsson) em dueto com Nicole Kidman. A música foi feita para um comercial de TV que depois ficou bem conhecido.</p>
+      </div>
+      <hr />
+      <div class="post">
+        <h3 class="titulo"><a href="#" title="Leia o post completo">Podsemfio n.47 - MP3 em celulares (parte I)</a></h3>
+        <p class="data"><span class="dia">11</span><span class="separador">/</span><span class="mes">Set</span><span class="separador">/</span><span class="ano">2007</span></p>
+        <p>Quando se trata de casar celular com diversão, ninguém bate a Sony Ericsson. A melhor conectividade com PDAs. O <a href="#">MP3 player da Ivete Sangalo</a>. Depois da linha Walkman e da linha Cybershot, vem aí mais uma linha de celular com a grife de uma linha de produtos Sony. Alguém sabe qual é?</p>
+        <p>Encerrando com "Something Stupid", com Robbie Williams (garoto-propaganda da Sony Ericsson) em dueto com Nicole Kidman. A música foi feita para um comercial de TV que depois ficou bem conhecido.</p>
+      </div>
+    </div>
+    <!-- FIM DE POSTS -->
+    <hr />
+    <!-- SOBRE -->
+    <div id="Sobre">
+      <h2>Meu Nome é Bia Kunze</h2>
+      <p>O PodSemFio é um podcast semanal sobre a tecnologia móvel no dia-a-dia, feito 100% em dispositivos portáteis! Acompanhe também minha rotina wireless no blog <a href="http://www.odontopalm.com.br/gsf/" title="Blog da Garota Sem Fio">Garota Sem Fio</a>.</p>
+    </div>
+    <!-- FIM DE SOBRE -->
+    <hr />
+    <!-- ASSINE -->
+    <div id="Assine">
+      <h2>Assine o <em>Podcast</em></h2>
+      <ul class="rss">
+        <li><a href="#" title="assine a versão normal do PodSemFio"><img src="img/rss_normal.jpg" alt="RSS Normal" /> Normal (80kbps)</a></li>
+        <li><a href="#" title="assine a versão low do PodSemFio"><img src="img/rss_low.jpg" alt="RSS Low" /> Low (16kbps)</a></li>
+      </ul>
+      <ul class="servicos">
+        <li><a href="#" title="assine o PodSemFio no Podnova"><img src="http://www.odontopalm.com.br/psf/podnova.gif" alt="podnova" /></a></li>
+        <li><a href="#" title="assine o PodSemFio no Podnova"><img src="http://www.odontopalm.com.br/psf/myyahoo.gif" alt="podnova" /></a></li>
+        <li><a href="#" title="assine o PodSemFio no Podnova"><img src="http://www.odontopalm.com.br/psf/itunes.jpg" alt="podnova" /></a></li>
+        <li><a href="#" title="assine o PodSemFio no Podnova"><img src="http://www.odontopalm.com.br/psf/newsgator.gif" alt="podnova" /></a></li>
+        <li><a href="#" title="assine o PodSemFio no Podnova"><img src="http://www.odontopalm.com.br/psf/odeo.gif" alt="podnova" /></a></li>
+        <li><a href="#" title="assine o PodSemFio no Podnova"><img src="http://www.odontopalm.com.br/psf/podnova.gif" alt="podnova" /></a></li>
+      </ul>
+    </div>
+    <!-- FIM DE ASSINE -->
+    <hr />
+    <!-- SIDEBAR -->
+    <div id="Sidebar">
+      <!-- RECENTES -->
+      <div id="Recentes">
+        <h2>Podcasts <em>Recentes</em></h2>
+        <ul>
+          <li><a href="#" title="link para o post completo">Podsemfio n.47 - MP3 em celulares (parte I)</a></li>
+          <li><a href="#" title="link para o post completo">Podsemfio n.47 - MP3 em celulares (parte I)</a></li>
+          <li><a href="#" title="link para o post completo">Podsemfio n.47 - MP3 em celulares (parte I)</a></li>
+          <li><a href="#" title="link para o post completo">Podsemfio n.47 - MP3 em celulares (parte I)</a></li>
+          <li><a href="#" title="link para o post completo">Podsemfio n.47 - MP3 em celulares (parte I)</a></li>
+          <li><a href="#" title="link para o post completo">Podsemfio n.47 - MP3 em celulares (parte I)</a></li>
+        </ul>
+      </div>
+      <!-- FIM DE RECENTES -->
+      <hr />
+      <!-- ARQUIVO -->
+      <div id="Arquivo">
+        <h2>Arquivos <em>Sem Fio</em></h2>
+        <ul>
+          <li><a href="#" title="todos os posts do mês">Setembro de 2007</a></li>
+          <li><a href="#" title="todos os posts do mês">Setembro de 2007</a></li>
+          <li><a href="#" title="todos os posts do mês">Setembro de 2007</a></li>
+          <li><a href="#" title="todos os posts do mês">Setembro de 2007</a></li>
+          <li><a href="#" title="todos os posts do mês">Setembro de 2007</a></li>
+          <li><a href="#" title="todos os posts do mês">Setembro de 2007</a></li>
+        </ul>
+      </div>
+      <!-- FIM DE ARQUIVO -->
+    </div>
+    <!-- FIM DE SIDEBAR -->
+  </div>
+  <!-- FIM DE CONTEUDO -->
+  <hr />
+  <!-- RODAPE -->
+  <div id="Rodape">
+    <div class="vcard">Blog criado por <a class="url fn" href="http://www.odontopalm.com.br/gsf" title="blog da Garota Sem Fio">Beatriz Kunze</a>
+      <div class="org">Site desenvolvido por <a href="http://www.teoriaglacial.com" title="confira o webfólio">Marcelo Glacial</a></div>
+    </div>
+  </div>
+  <!-- FIM DE RODAPE -->
+</div>
+<!-- FIM DE SITE -->
+</body>
+</html>
